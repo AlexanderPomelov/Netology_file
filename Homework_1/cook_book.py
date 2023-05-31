c3f4f1bfe11698d7d23f2b36f25553d5f741a247
AlexanderPomelov <129675273+AlexanderPomelov@users.noreply.github.com>
2023-05-31 20:30:02 +0300

    Update cook_book.py

--- Homework_1/cook_book.py
+++ Homework_1/cook_book.py
@@ -1,15 +1,15 @@
 import json
 
 
-with open (r'C:\Users\apomelov\Desktop\TEST\Homework_file\Homework_1\recipes.txt', encoding='utf-8') as file:
+with open (r'C:\Users\apomelov\Desktop\TEST\Homework_file\Homework_1\recipes.txt', 'rt', encoding='utf-8') as file:
     cook_book = {}
     for recipes in file:
         dish_name = recipes.strip()
-        cook_ingridient = int(file.readline())
+        cook_ingredient = int(file.readline())
         cook_list = []
-        for i in range(cook_ingridient):
-            ingridient_name, quantity, measure = file.readline().strip().split('|')
-            cook_list.append({ 'ingridient_name': ingridient_name, 'quantity': quantity, 'measure': measure })
+        for i in range(cook_ingredient):
+            ingredient_name, quantity, measure = file.readline().strip().split('|')
+            cook_list.append({ 'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure })
         file.readline()
 
         cook_book[dish_name] = cook_list
